726548db2605f3050398bf665190ed7f26e5c8bf
hechao <499187993@qq.com>
2018-10-23 16:36:24 +0800

    第7章结束

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Blog, BlogType
 # Register your models here.
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'author', 'created_time', 'last_updated_time',)
+@admin.register(BlogType)
+class BlogTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type_name')
